ada9d5f99b6e06f4dc0ffcb18439bbb415c4486e
nicky_ru <nikitkaruban@gmail.com>
2021-07-06 21:36:37 +0800

    small fixes

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,5 +1,5 @@
 const DriveSlowSafe = artifacts.require("DriveSlowSafe");
 
 module.exports = function (deployer) {
-    deployer.deploy(DriveSlowSafe);
+    deployer.deploy(DriveSlowSafe, {from: "0xF26a43cb8FF1fa90b603152B845A3E2de9c5Ba6F", value: 1000000000000000000});
 };
