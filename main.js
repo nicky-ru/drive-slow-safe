00af10583ac9462c1803345ff7d12a394e0a612c
nicky_ru <nikitkaruban@gmail.com>
2021-07-10 17:34:08 +0800

    add haversine formula

--- main.js
+++ main.js
@@ -566,6 +566,24 @@ const bucketName = "vactracker";
 
 const datapoints = {};
 
+function calcDistance(lat1, lat2, lon1, lon2) {
+    let R = 6371; // Radius of the earth in km
+    let dLat = deg2rad(lat2-lat1);
+    let dLon = deg2rad(lon2-lon1);
+    let a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+    ;
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    let d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
+
 async function sendDataToSC() {
     for (let datapoint in datapoints) {
         if (!datapoints[datapoint].processed) {
